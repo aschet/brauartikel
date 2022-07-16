34722216bf295f22dc5c1772c296d0b2a03b9956
Thomas Ascher <thomas.ascher@gmx.at>
2022-07-16 14:47:50 +0200

    added content

--- bierfarbe/beer_colour.py
+++ bierfarbe/beer_colour.py
@@ -1,17 +1,11 @@
-# Philip Lee's approximation from a color swatch and curve fitting.
-# http://www.brewtarget.org/
-
 EBC_SCALE = [1.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0]
 
-def ebc_to_rgb(ebc):
-    srm = ebc / 1.97
-    r = round(min(253, max(0, 0.5 + (272.098 - 5.80255 * srm))))
-    if srm > 35.0:
-        g = 0.0
-    else:
-        g = round(min(255, max(0, 0.5 + (2.41975e2 - 1.3314e1 * srm + 1.881895e-1 * srm * srm))))
-    b = round(min(255, max(0, 0.5 + (179.3 - 28.7 * srm))))
-    return '#%02x%02x%02x' % (int(r), int(g), int(b))
+def ebc_to_rgb(EBC):
+    # observer=CIE 1964 10 Degree Standard Observer, illuminant=D65, path=7.5cm
+    R=round(max(0, min(255, (-4.8043E-06*EBC**4)+(4.6721E-04*EBC**3)+(3.6423E-02*EBC**2)+(-6.4444E+00*EBC)+(2.6192E+02))))
+    G=round(max(0, min(255, (2.1860E-05*EBC**4)+(-4.9979E-03*EBC**3)+(4.3213E-01*EBC**2)+(-1.6826E+01*EBC)+(2.4569E+02))))
+    B=round(max(0, min(255, (1.1796E-07*EBC**6)+(-3.2688E-05*EBC**5)+(3.5723E-03*EBC**4)+(-1.9423E-01*EBC**3)+(5.4069E+00*EBC**2)+(-6.9262E+01*EBC)+(2.6361E+02))))
+    return '#%02x%02x%02x' % (int(R), int(G), int(B))
 
 for ebc in EBC_SCALE:
     print('{} -> {}'.format(ebc, ebc_to_rgb(ebc)))
---
+++ bierfarbe/makeebcscale.py
@@ -0,0 +1,83 @@
+# Beer EBC to sRGB Model Generator
+# Copyright 2022 Thomas Ascher
+# SPDX-License-Identifier: MIT
+# Run within Jupyter Notebook
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import colour
+import colour.plotting
+
+# Adjust the following constants to alter the generated model
+BEER_GLAS_DIAMETER_CM = 7.5 # 7.5 is average diameter of a Teku glas
+OBSERVER_NAME = 'CIE 1964 10 Degree Standard Observer'
+ILLUMINANT_NAME = 'D65'
+MAX_EBC = 80
+POLY_DEGREE_R = 4
+POLY_DEGREE_G = 4
+POLY_DEGREE_B = 6
+
+OBSERVER = colour.MSDS_CMFS[OBSERVER_NAME]
+ILLUMINANT = colour.SDS_ILLUMINANTS[ILLUMINANT_NAME]
+ILLUMINANT_XY = colour.CCS_ILLUMINANTS[OBSERVER_NAME][ILLUMINANT_NAME]
+ASBC_SHAPE = colour.SpectralShape(380, 780, 5)
+
+def transmission_sd(ebc, path_cm):
+    srm = ebc / 1.94
+    wl = ASBC_SHAPE.range()
+    values = 10**(-(srm / 12.7) * (0.018747 * math.e**(-(wl - 430.0) / 13.374) + 0.98226 * math.e** (-(wl - 430.0) / 80.514)) * path_cm)
+    return colour.SpectralDistribution(values, wl)
+
+EBC = np.arange(0, MAX_EBC, dtype='int')
+RGB = []
+for i in EBC:
+    XYZ = colour.sd_to_XYZ(transmission_sd(i, BEER_GLAS_DIAMETER_CM), cmfs=OBSERVER, illuminant=ILLUMINANT) / 100.0
+    RGB.append(colour.XYZ_to_sRGB(XYZ, illuminant=ILLUMINANT_XY))
+
+fig_scale, ax_scale = colour.plotting.plot_multi_colour_swatches([colour.plotting.ColourSwatch(RGB=np.clip(i, 0, 1)) for i in RGB], columns=len(EBC), **{'standalone': False})
+ax_scale.xaxis.set_label_text('EBC')
+ax_scale.xaxis.set_ticks_position('bottom')
+#fig_scale.savefig('colorscale.pdf', format='pdf')
+
+R = [ 255 * i[0] for i in RGB]
+G = [ 255 * i[1] for i in RGB]
+B = [ 255 * i[2] for i in RGB]
+
+R_COEFF = np.polyfit(EBC, R, POLY_DEGREE_R)
+G_COEFF = np.polyfit(EBC, G, POLY_DEGREE_G)
+B_COEFF = np.polyfit(EBC, B, POLY_DEGREE_B)
+
+fig_model = plt.figure()
+ax_model = fig_model.subplots(1)
+ax_model.set_title('sRGB Spectrum for Average Beers at ' + '{:.1f}'.format(BEER_GLAS_DIAMETER_CM)
++ ' cm Glas Diameter\nwith ' + OBSERVER_NAME + ' and ' + ILLUMINANT_NAME + ' Illuminant')
+ax_model.xaxis.set_label_text('EBC')
+ax_model.yaxis.set_label_text('Channel Intensity')
+
+def plot_channel(values, coeff, color, label):
+    ax_model.plot(EBC, values, color=color, label=label)
+    ax_model.plot(EBC, np.poly1d(coeff)(EBC), color=color, label='Model ' + label, linestyle=':')
+
+plot_channel(R, R_COEFF, '#ff0000', 'R')
+plot_channel(G, G_COEFF, '#00ff00', 'G')
+plot_channel(B, B_COEFF, '#0000ff', 'B')
+ax_model.legend()
+
+def print_poly(name, coeff):
+    parts = []
+    exp = len(coeff) - 1
+    for i in coeff:
+        multiplier = ''
+        if exp == 1:
+            multiplier = '*EBC'
+        elif exp > 1:
+            multiplier = '*EBC**' + str(exp)
+        parts.append('(' + '{:.4E}'.format(i) + multiplier + ')')
+        exp = exp - 1
+    print(name + '=round(max(0, min(255, ' + '+'.join(parts) + ')))')
+
+print('Model:')
+print('# observer=' + OBSERVER_NAME + ', illuminant=' + ILLUMINANT_NAME + ', path=' + str(BEER_GLAS_DIAMETER_CM) + 'cm')
+print_poly('R', R_COEFF)
+print_poly('G', G_COEFF)
+print_poly('B', B_COEFF)
