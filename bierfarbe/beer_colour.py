4f63fe92f4f98eeaa9700b1fedcecd11e90f861b
Thomas Ascher <thomas.ascher@gmx.at>
2022-07-17 21:12:00 +0200

    use plot

--- bierfarbe/beer_colour.py
+++ bierfarbe/beer_colour.py
@@ -1,12 +1,16 @@
-EBC_SCALE = [1.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0]
+import numpy as np
+import colour.plotting
 
 def ebc_to_rgb(ebc):
     srm = ebc / 1.94
     # SRM to sRGB model for 7.5 cm glas diameter
-    r=round(max(0.0, min(255.0, 255.0*(1.017e+00+srm*(-4.587e-02+srm*(2.848e-04+srm*(2.162e-05+srm*(-3.604e-07))))))))
-    g=round(max(0.0, min(255.0, 255.0*(9.723e-01+srm*(-1.292e-01+srm*(6.489e-03+srm*(-1.477e-04+srm*(1.277e-06))))))))
-    b=round(max(0.0, min(255.0, 255.0*(1.022e+00+srm*(-5.359e-01+srm*(8.222e-02+srm*(-5.791e-03+srm*(2.088e-04+srm*(-3.745e-06+srm*(2.650e-08))))))))))
-    return '#%02x%02x%02x' % (int(r), int(g), int(b))
+    r=max(0.0, min(1.0, 1.017e+00+srm*(-4.587e-02+srm*(2.848e-04+srm*(2.162e-05+srm*(-3.604e-07))))))
+    g=max(0.0, min(1.0, 9.723e-01+srm*(-1.292e-01+srm*(6.489e-03+srm*(-1.477e-04+srm*(1.277e-06))))))
+    b=max(0.0, min(1.0, 1.022e+00+srm*(-5.359e-01+srm*(8.222e-02+srm*(-5.791e-03+srm*(2.088e-04+srm*(-3.745e-06+srm*(2.650e-08))))))))
+    return [r, g, b]
 
-for ebc in EBC_SCALE:
-    print('{} -> {}'.format(ebc, ebc_to_rgb(ebc)))
+ebc = np.arange(start=0, stop=80+1, dtype='int')
+
+fig_scale, ax_scale = colour.plotting.plot_multi_colour_swatches([colour.plotting.ColourSwatch(RGB=ebc_to_rgb(i)) for i in ebc], **{'standalone': False})
+ax_scale.xaxis.set_label_text('EBC')
+ax_scale.xaxis.set_ticks_position('bottom')
